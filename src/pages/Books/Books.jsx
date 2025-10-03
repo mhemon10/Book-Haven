2518f0779eec4f5c71da50fede3151256b0ba2bc
MH Emon <mhemon@MHs-MacBook-Air.local>
2025-10-03 23:45:43 +0600

    Data fetching

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useState } from 'react';
+import Book from '../Book/book';
 
-const Books = () => {
+const Books = ({data}) => {
 
-    const [allBooks, setallBooks] = useState([])
+    const [allBooks, setAllBooks] = useState([])
     
-    useEffect(() => {
-        fetch("booksData.json")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-            })
-    },[])
+    // useEffect(() => {
+    //     fetch("booksData.json")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setAllBooks(data)
+    //         })
+    // },[])
     return (
         <div>
             <h1>hi ami Books</h1>
+            <Suspense fallback={<span>Loading....</span>}>
+                {
+                    data.map((singleBook=>))
+            }
+            
+            </Suspense>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Category from '../../components/Category/Category';
 import Books from '../Books/Books';
+import { useLoaderData } from 'react-router';
 
 const Home = () => {
+
+    const data = useLoaderData();
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
-            <Books></Books>
+            <Books data ={data}></Books>
         </div>
     );
 };
